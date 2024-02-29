8142c93b0e00f937f89e81b09739d5695ec80c4d
Marcelo Clementino <marcelo.clementino@uol.com.br>
2024-02-28 21:11:52 -0300

    Atualiza assets das operadoras

--- cmsBase/vivo/assets/js/util.js
+++ cmsBase/vivo/assets/js/util.js
@@ -1,3 +1,18 @@
+let pressTimer;
+
+var clickQuant = 0;
+function startTimer() {
+    clickQuant++;
+    if (clickQuant == 5) {
+        window.location.href = '../index.html';
+    }
+}
+
+function clearTimer() {
+    clearTimeout(pressTimer);
+}
+
+
 $(window).on('load', function() {
     // Oculte o overlay de carregamento
     $('#loading-overlay').css('opacity', 0);
@@ -8,13 +23,53 @@ $(window).on('load', function() {
     });
 });
 
+$(document).ready(function() {
+    var videos = $(".video-sequence").toArray();
+
+    function playNextVideo(index) {
+        if (index < videos.length) {
+            var video = videos[index];
+            var overlay = $(video).prev('.video-overlay');
+
+            // Inicia o vídeo
+            video.play();
+
+            // Define um atraso antes de esconder a overlay
+            setTimeout(function() {
+                overlay.hide();
+            }, 2000); // Atraso de 1000 milissegundos (1 segundo)
+
+            // Quando o vídeo atual termina, chama a função para o próximo vídeo
+            $(video).on('ended', function() {
+                // Se ainda há vídeos na sequência, toca o próximo
+                if (index + 1 < videos.length) {
+                    playNextVideo(index + 1);
+                } else {
+                    // Se é o último vídeo, reinicia a sequência
+                    playNextVideo(0);
+                }
+            });
+        }
+    }
+
+    // Inicia a reprodução do primeiro vídeo
+    playNextVideo(0);
+});
+
+
+
+ // Função que será chamada após 40 segundos
+ function redirect() {
+    window.location.href = 'index.html';
+}
+
+// Definindo o redirecionamento para daqui a 40 segundos (40000 milissegundos)
+//setTimeout(redirect, 60000);
+
+
 $(document).ready(function(){
     $('.nav-link').click(function(){
         $('.seta-img').css('visibility', 'hidden'); // Oculta todas as setas
         $(this).closest('li').find('.seta-img').css('visibility', 'visible'); // Exibe a seta dentro do botão clicado
     });
 });
-
-function formatCurrency(number) {
-    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(number);
-}
