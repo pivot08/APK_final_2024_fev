9a33c1490eed17f0d61b01748e22179769197806
Pivot 08 <patricia@pivot08.com.br>
2024-04-24 13:03:49 -0300

    ajustes cliente

--- cmsBase/picms/func/menu.php
+++ cmsBase/picms/func/menu.php
@@ -16,16 +16,13 @@
 		</div>
 		<nav class="main-menu">
 			<ul class="nav metismenu">
-			
-				<li class="sidebar-header"><a href="index.php" aria-expanded="false"><i
-								class="icon dripicons-graduation"></i><span>TUTORIAIS</span></a></li>
-				<li class="sidebar-header"><span>CONTEÚDOS</span></li>
+				
 				<?php if ($_SESSION['user']['UserTypeID'] == 1) { ?>
 					<li
-						class="nav-dropdown <?php echo ($urlPage == 'template' || $urlPage == 'template-list' || $urlPage == 'template-content' || $urlPage == 'template-content-list' || $urlPage == 'template-content-home' || $urlPage == 'template-content-buttons' || $urlPage == 'template-content-text' || $urlPage == 'template-content-product-line' || $urlPage == 'template-content-home-feature' || $urlPage == 'template-content-step-by-step' ? 'active' : ''); ?>">
-						<a class="has-arrow" href="#" aria-expanded="false"><i
-								class="icon dripicons-duplicate"></i><span>CONTEÚDO</span></a>
-						<ul class="collapse nav-sub">
+						class="nav-dropdown <?php echo ($urlPage == 'template' || $urlPage == 'template-list' || $urlPage == 'template-content' || $urlPage == 'template-content-list' || $urlPage == 'template-content-home' || $urlPage == 'template-content-buttons' || $urlPage == 'template-content-text' || $urlPage == 'template-content-product-line' || $urlPage == 'template-content-home-feature' || $urlPage == 'template-content-step-by-step' ? 'active' : 'active'); ?>">
+						<a class="has-arrow" href="#" aria-expanded="true"><i
+								class="icon dripicons-meter"></i><span>CONTEÚDO</span></a>
+						<ul class=" nav-sub">
 							<li id="template-list" <?php echo ($urlPage == 'template-list' ? 'class="active"' : ''); ?>><a
 									href="template-list.php"><span>Criar páginas (templates)</span></a></li>
 							<li id="template-content-list" <?php echo ($urlPage == 'template-content-text' || (isset($pageTypeID) && $pageTypeID == "3") ? 'class="active"' : ''); ?>><a
@@ -46,9 +43,9 @@
 				<?php } ?>
 				<li
 					class="nav-dropdown <?php echo ($urlPage == 'operator-user' || $urlPage == 'operator-user-list' || $urlPage == 'operator-template' || $urlPage == 'operator-template-list' || $urlPage == 'device' || $urlPage == 'device-list' || $urlPage == 'operator-plan' || $urlPage == 'operator-plan-list' || $urlPage == 'operator-value' || $urlPage == 'operator-value-list' ? 'active' : ''); ?>">
-					<a class="has-arrow" href="#" aria-expanded="false"><i class="icon dripicons-user-id"></i><span>CONTEÚDO
+					<a class="has-arrow" href="#" aria-expanded="false"><i class="icon dripicons-meter"></i><span>CONTEÚDO
 							OPERADORAS</span></a>
-					<ul class="collapse nav-sub">
+					<ul class=" nav-sub">
 					<li id="template-user-list" <?php echo ($urlPage == 'template-user' || $urlPage == 'template-user-list' ? 'class="active"' : ''); ?>><a href="template-user-list.php"><span>Usuários p/ operadoras</span></a></li>	
 					<li id="operator-user-list" <?php echo ($urlPage == 'operator-user' || $urlPage == 'operator-user-list' ? 'class="active"' : ''); ?>><a href="operator-user-list.php"><span>Usuários</span></a></li>
 						<li id="operator-template-list" <?php echo ($urlPage == 'operator-template' || $urlPage == 'operator-template-list' ? 'class="active"' : ''); ?>><a
@@ -62,9 +59,9 @@
 				</li>
 				<?php if ($_SESSION['user']['UserTypeID'] == 1) { ?>
 					<li
-						class="nav-dropdown <?php echo ($urlPage == 'update-site' || $urlPage == 'update-tablet' ? 'active' : ''); ?>">
-						<a class="has-arrow" href="#" aria-expanded="false"><i
-								class="icon dripicons-upload"></i><span>ATUALIZAÇÃO</span></a>
+						class="nav-dropdown <?php echo ($urlPage == 'update-site' || $urlPage == 'update-tablet' ? 'active' : 'active'); ?>">
+						<a class="has-arrow" href="#" aria-expanded="true"><i
+								class="icon dripicons-meter"></i><span>ATUALIZAÇÃO</span></a>
 						<ul class="collapse nav-sub">
 							<li id="update-site" <?php echo ($urlPage == 'update-site' ? 'class="active"' : ''); ?>><a
 									href="update-site.php"><span>Atualizar Site</span></a></li>
@@ -75,8 +72,8 @@
 				<?php } ?>
 				<?php if ($_SESSION['user']['UserTypeID'] == 1) { ?>
 					<li class="nav-dropdown <?php echo ($urlPage == 'navigation-control' ? 'active' : ''); ?>">
-						<a class="has-arrow" href="#" aria-expanded="false"><i
-								class="icon dripicons-document"></i><span>RELATÓRIO</span></a>
+						<a class="has-arrow" href="#" aria-expanded="true"><i
+								class="icon dripicons-meter"></i><span>RELATÓRIO</span></a>
 						<ul class="collapse nav-sub">
 							<li id="navigation-control" <?php echo ($urlPage == 'navigation-control' ? 'class="active"' : ''); ?>><a
 									href="navigation-control.php"><span>Registro de Navegação</span></a></li>
