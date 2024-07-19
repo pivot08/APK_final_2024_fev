a50fefaede152bf7913eede768f3aabfeaa99687
Marcelo Clementino <marcelo.clementino@uol.com.br>
2024-07-19 17:07:37 -0300

    Austes registro de navegação

--- cmsBase/picms/func/navigation.php
+++ cmsBase/picms/func/navigation.php
@@ -3,12 +3,12 @@
 if (
    isset($_POST['version'])
    && isset($_POST['actionID'])
-   && isset($_POST['templateID'])
-   && isset($_POST['templateContentID'])
    && isset($_POST['actionDate'])
    && isset($_POST['deviceId'])
    && isset($_POST['deviceModel'])
    && isset($_POST['deviceManufacturer'])
+   && (isset($_POST['templateID'])
+   || isset($_POST['templateContentID']))
 ) {
    insertNavigationControl($_POST['version'], $_POST['actionID'], $_POST['templateID'], $_POST['templateContentID'], $_POST['actionDate'], $_POST['deviceId'], $_POST['deviceModel'], $_POST['deviceManufacturer']);
 }
@@ -16,12 +16,12 @@ if (
 if (
    isset($_GET['version'])
    && isset($_GET['actionID'])
-   && isset($_GET['templateID'])
-   && isset($_GET['templateContentID'])
    && isset($_GET['actionDate'])
    && isset($_GET['deviceId'])
    && isset($_GET['deviceModel'])
    && isset($_GET['deviceManufacturer'])
+   && (isset($_GET['templateID'])
+   || isset($_GET['templateContentID']))
 ) {
    insertNavigationControl($_GET['version'], $_GET['actionID'], $_GET['templateID'], $_GET['templateContentID'], $_GET['actionDate'], $_GET['deviceId'], $_GET['deviceModel'], $_GET['deviceManufacturer']);
 }
